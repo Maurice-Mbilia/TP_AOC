8580002e029c0e3e4e002507ca91ddfa9965d3d6
Maurice Mbilia <maurice.mbilia@etudiant.univ-rennes1.fr>
2021-12-28 16:36:09 +0100

    first commit

---
+++ src/class_impl/Canal.java
@@ -0,0 +1,14 @@
+package class_impl;
+
+import implementation_interfaces.Observer;
+import implementation_interfaces.Subject;
+
+public class Canal implements Observer {
+
+	@Override
+	public void update(Subject subject) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
