6df52327f211acbf8bb2a6c323ec4c91b70e5cc2
Maurice Mbilia <maurice.mbilia@etudiant.univ-rennes1.fr>
2021-12-31 15:39:59 +0100

    configuration junit 5

---
+++ test/class_impl/DiffusionAtomiqueTest.java
@@ -0,0 +1,14 @@
+package class_impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DiffusionAtomiqueTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
